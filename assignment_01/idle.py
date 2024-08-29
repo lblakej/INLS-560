91079e044df7d77c750811ef96a085e88606c88a
lblakej <32578870+lblakej@users.noreply.github.com>
2024-08-28 20:33:44 -0400

    Add files via upload

---
+++ assignment_01/idle.py
@@ -0,0 +1,18 @@
+'''
+Python 3.12.5 (v3.12.5:ff3bc82f7c9, Aug  7 2024, 05:32:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+my_var = Devin
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    my_var = Devin
+NameError: name 'Devin' is not defined
+my_var = 'Devin'
+print (my_var)
+Devin
+my_var = "Dani"
+print(my_var)
+Dani
+
+
+
+'''
