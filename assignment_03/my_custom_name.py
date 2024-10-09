3bcf676af527940567ea2b210dc355884c5a1198
lblakej <32578870+lblakej@users.noreply.github.com>
2024-10-09 10:16:09 -0400

    Update my_custom_name.py

--- assignment_03/my_custom_name.py
+++ assignment_03/my_custom_name.py
@@ -1,1 +1,1 @@
-# you may want to name your assignment main.py. Don't name it cars.py. Name it to reflect what your code is about.
+# You may want to name your assignment main.py. Don't name it cars.py. Name it to reflect what your code is about.
